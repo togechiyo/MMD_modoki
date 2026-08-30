163538f10925b4e8409268499b4095eec8116118
togechiyo <togechiyo.gmail.com>
2026-08-30 20:21:41 +0900

    Include license notices in packaged app

--- forge.config.ts
+++ forge.config.ts
@@ -11,6 +11,10 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource: [
+      'LICENSE',
+      'THIRD_PARTY_NOTICES.md',
+    ],
   },
   rebuildConfig: {},
   makers: [
